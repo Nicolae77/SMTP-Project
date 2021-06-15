51af46ddbcdcb770a67560bc01d92bd4c27e5b30
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-06-15 13:24:33 +0100

    Update main.py

--- SMTP Project/main.py
+++ SMTP Project/main.py
@@ -7,7 +7,7 @@ connection.starttls()  # a way of securing our connection
 connection.login(user=my_email, password=password)
 connection.sendmail(
     from_addr=my_email,
-    to_addrs="bogdancaldarasan787@gmail.com",
+    to_addrs="steve@gmail.com",
     msg="Subject:Hello\n\n This is the body of my email")
 
 connection.close()
